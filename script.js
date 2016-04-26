7a69b9f7f18d4be8d879637a2357cc51b82c7eb8
Eugene Kwon <ekwon86@gmail.com>
2016-04-26 11:32:38 -0700

    calculator v0.5 - Eugene Kwon

--- script.js
+++ script.js
@@ -23,3 +23,20 @@ function callback(type, value, item) {
             break;
     }
 }
+//my_calculator - creates a new calculator object 
+var my_calculator = new calculator(callback);
+//after DOM load add click handlers to all buttons
+$(document).ready(function() {
+   $('button').on('click', function() {
+       var val = $(this).text();
+       console.log("btn clicked : ", val);
+       switch (val) {
+           case 'AC':
+               my_calculator.allClear();
+               break;
+           default: 
+               my_calculator.addItem($(this).text());
+               break;
+       }
+   }) 
+});
