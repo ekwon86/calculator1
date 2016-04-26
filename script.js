ad2241c424021e400bdebec37044a841a9314f8c
Eugene Kwon <ekwon86@gmail.com>
2016-04-26 15:38:52 -0700

    memory_match v0.5 - Eugene Kwon

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-/**
- * Created by gkwon on 4/25/16.
- */
+// Created by gkwon on 4/25/16.
 
-
-// var display = $('.display');
-// var input1 = '';
-//
-// $('.keys').click(function(){
-//     input1 += $(this).html();
-//     display.html(input1);
-//     console.log(input1);
-// });
 
 //callback function
 function callback(type, value, item) {
