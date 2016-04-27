66d89556da3c16d6b7296c0e8eec212db764fd2b
Eugene Kwon <ekwon86@gmail.com>
2016-04-27 14:46:42 -0700

    calculator v1 - Eugene Kwon

--- script.js
+++ script.js
@@ -1,33 +1,70 @@
 // Created by gkwon on 4/25/16.
 
 //callback function
-function callback(type, value, item) {
-    switch (value) {
-        case undefined:
-            $('.display').html(" ");
-            break;
-        default:
-            $('.display').html(value);
-            break;
-    }
-}
-//my_calculator - creates a new calculator object 
-var my_calculator = new calculator(callback);
+// function callback(type, value, item) {
+//     switch (value) {
+//         case undefined:
+//             $('.display').html(" ");
+//             break;
+//         default:
+//             $('.display').html(value);
+//             break;
+//     }
+// }
+//my_calculator - creates a new calculator object
+// var my_calculator = new calculator(callback);
 //after DOM load add click handlers to all buttons
+
+//global variables
+var num_array = [' ', ' '];
+var operator = ' ';
+var index = 0;
+
 $(document).ready(function() {
-   $('button').on('click', function() {
+
+   
+    $('button').on('click', function() {
        var val = $(this).text();
        console.log("btn clicked : ", val);
        switch (val) {
+           case '+':
+
+               
+               break;
+           case '-':
+
+              
+               break;
+           case '÷':
+
+              
+               break;
+           case 'x':
+
+               
+               break;
            case 'AC':
-               my_calculator.allClear();
+
                break;
            case 'C':
-               my_calculator.clear();
+
                break;
-           default: 
-               my_calculator.addItem($(this).text());
+           default:
                break;
        }
-   }) 
+   })
 });
+
+// if number is pressed add to current position
+
+
+
+
+/*-------------------- V1.0 --------------------*/
+//add values from buttons clicked into plain object that has the following properties
+    //type - equal to one of these types "number", "operator", "equalSign"
+    //value - equal to the value of the button
+//add object into a global array variable
+//process calculation based on objects in array
+
+
