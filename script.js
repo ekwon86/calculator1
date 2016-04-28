6748bf661cf863bb0b7b05368eaab51d814899ec
Eugene Kwon <ekwon86@gmail.com>
2016-04-28 15:23:06 -0700

    calculator v1.1 - Eugene Kwon

--- script.js
+++ script.js
@@ -7,98 +7,162 @@ var operator = ' ';
 var index = 0;
 
 $(document).ready(function() {
-    //set initial display to '0'
-    $('.display').html("0");
+    //click handler for numbers
+    $('button.numbers').click(function(){
+        process_number_click(this);
+    });
 
-    // i can click zero numerous times. figure out a way to prevent 0 from logging more than once if value is 0.
+    function process_number_click(the_button){
+        var val = $(the_button).text();
+        num_array[index] += val;
+        $('.display').html(num_array[index]);
+        console.log(num_array);
+    }
 
-    // //click handler for buttons
-    // $('button.numbers').click(function(){
-    //     process_number_click(this);
-    // });
-    //
-    // function process_number_click(the_button){
-    //     var val = $(the_button).text();
-    //     num_array[index] += val;
-    //     $('.display').html(num_array[index]);
-    //     console.log(num_array);
-    // }
-    $('button').on('click', function() {
-        var val = $(this).text();
-        console.log("btn click: ", val);
+    // click handler for decimal
+    $('button.decimal').click(function() {
+        process_decimal_click(this);
+    });
 
-        //numbers TODO:  this should go into a click handler for button with a class of number
-        if ($(this).hasClass('numbers')) {
-            num_array[index] += val;
-            $('.display').html(num_array[index]);
-            console.log(num_array);
+    function process_decimal_click(the_button){
+        var val = $(the_button).text();
+        num_array[index] += val;
+        $('.display').html(num_array[index]);
+    }
+
+    //click handler for operators
+    $('button.operators').click(function() {
+        process_operator_click(this);
+    });
+
+    function process_operator_click(the_button){
+        var val = $(the_button).text();
+        operator = val;
+        index++;
+        $('.display').html(val);
+    }
+
+    //click handler for all-clear
+    $('button.all-clear').click(function() {
+        process_allclear_click(this);
+    });
+    function process_allclear_click(the_button){
+        var val = $(the_button).text();
+        num_array = [' ', ' '];
+        operator = ' ';
+        index = 0;
+        $('.display').html(" ");
+        console.log(num_array, operator);
+    }
+
+    //click handler for clear
+    $('button.clear').click(function() {
+        process_clear_click(this);
+    });
+    function process_clear_click(the_button){
+        var val = $(the_button).text();
+        $(num_array[index]).val(" ");
+        $('.display').html(" ");
+    }
+
+    //click handler for equals
+    $('button.equals').click(function() {
+        process_equals_click(this);
+    });
+    function process_equals_click(the_button){
+        var val = $(the_button).text();
+        var answer = 0;
+        switch (operator) {
+            case '+':
+                //use parseFloat to parse numbers in case of decimals.
+                answer = parseFloat(num_array[0]) + parseFloat(num_array[1]);
+                break;
+            case '-':
+                answer = parseFloat(num_array[0]) - parseFloat(num_array[1]);
+                break;
+            case '÷':
+                answer = parseFloat(num_array[0]) / parseFloat(num_array[1]);
+                break;
+            case 'x':
+                answer = parseFloat(num_array[0]) * parseFloat(num_array[1]);
+                break;
+            default:
+                break;
         }
-        //decimal TODO: ths should go into a click handler for a button with a class of decimal
-        else if ($(this).hasClass('decimal')) {
-            num_array[index] += val;
-            $('.display').html(num_array[index]);
-        }
-        //operators
-        else if ($(this).hasClass('operators')) {
-            operator = val;
-            index++;
-            $('.display').html(val);
-        }
-        //all clear
-        else if ($(this).hasClass('all-clear')) {
-            num_array = [' ', ' '];
-            operator = ' ';
-            index = 0;
-            $('.display').html(" ");
-            console.log(num_array, operator);
-        }
-        //clear content in current array
-        else if ($(this).hasClass('clear')) {
-            $(num_array[index]).val(" ");
-            $('.display').html(" ");
-        }
-        //equals
-        else if ($(this).hasClass('equals')) {
-            var answer = 0;
-                switch (operator) {
-                    case '+':
-                        //use parseFloat to parse numbers in case of decimals.
-                        answer = parseFloat(num_array[0]) + parseFloat(num_array[1]);
-                        break;
-                    case '-':
-                        answer = parseFloat(num_array[0]) - parseFloat(num_array[1]);
-                        break;
-                    case '÷':
-                        //display set to error if you divide by 0.
-                        if (num_array[1] == 0) {
-                            $('.display').html('error');
-                        }
-                        else {
-                            answer = parseFloat(num_array[0]) / parseFloat(num_array[1]);
-                        }
-                        break;
-                    case 'x':
-                        answer = parseFloat(num_array[0]) * parseFloat(num_array[1]);
-                        break;
-                    case '=':
-                        // what happens when you press equals again?
-                        // figure out how to recall operator used from previous
-                            if (operator == '+') {
-                                answer = answer + parseFloat(num_array[0]) + parseFloat(num_array[1]);
-                            }
-                        break;
-                    default:
-
-                        break;
-                }
-            $('.display').html(answer);
-            console.log('the answer is: ' + answer);
-        }
-
-
-
-    })
+        $('.display').html(answer);
+        console.log('the answer is: ' + answer);
+    }
 });
+
+
+// //click handler for buttons
+// $('button').on('click', function() {
+//     var val = $(this).text();
+//
+//     //numbers TODO:  this should go into a click handler for button with a class of number
+//     if ($(this).hasClass('numbers')) {
+//         // process_number_click();
+//     }
+//     //decimal TODO: ths should go into a click handler for a button with a class of decimal
+//     else if ($(this).hasClass('decimal')) {
+//         // process_decimal_click();
+//     }
+//     //operators
+//     else if ($(this).hasClass('operators')) {
+//         // process_operator_click();
+//     }
+//     //all clear
+//     else if ($(this).hasClass('all-clear')) {
+//         num_array = [' ', ' '];
+//         operator = ' ';
+//         index = 0;
+//         $('.display').html(" ");
+//         console.log(num_array, operator);
+//     }
+//     //clear content in current array
+//     else if ($(this).hasClass('clear')) {
+//         $(num_array[index]).val(" ");
+//         $('.display').html(" ");
+//     }
+//equals
+// else if ($(this).hasClass('equals')) {
+//     var answer = 0;
+//         switch (operator) {
+//             case '+':
+//                 //use parseFloat to parse numbers in case of decimals.
+//                 answer = parseFloat(num_array[0]) + parseFloat(num_array[1]);
+//                 break;
+//             case '-':
+//                 answer = parseFloat(num_array[0]) - parseFloat(num_array[1]);
+//                 break;
+//             case '÷':
+//                 //display set to error if you divide by 0.
+//                 if (num_array[1] == 0) {
+//                     $('.display').html('error');
+//                 }
+//                 else {
+//                     answer = parseFloat(num_array[0]) / parseFloat(num_array[1]);
+//                 }
+//                 break;
+//             case 'x':
+//                 answer = parseFloat(num_array[0]) * parseFloat(num_array[1]);
+//                 break;
+//             case '=':
+//                 // what happens when you press equals again?
+//                 // figure out how to recall operator used from previous
+//                     if (operator == '+') {
+//                         answer = answer + parseFloat(num_array[0]) + parseFloat(num_array[1]);
+//                     }
+//                 break;
+//             default:
+//
+//                 break;
+//         }
+//     $('.display').html(answer);
+//     console.log('the answer is: ' + answer);
+// }
+//     })
+// });
 
 
 
